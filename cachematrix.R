414a80d9eef4036cc033337e507c234f7103a095
jsdll1 <joshua@tactiles.io>
2021-01-03 14:06:06 +0800

    Complete the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Minimize the time spent on calculating the inverse of a matrix by utilizeing
+## the cache
 
-## Write a short comment describing this function
+## Store a matrix and it's inverse in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  
+  set <- function (y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  
+  list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Solve the inverse of the matrix, or output it from the cache if available
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  data <- x$get()
+  i <- solve(data)
+  x$setInverse(i)
+  i
 }
